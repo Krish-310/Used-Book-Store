5e80e3f8b554a6004de255b49dd7ac4055920fb9
Krish Kochar <krishkochar@gmail.com>
2024-01-12 14:30:20 -0500

    Init Repo

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+    'mongodb+srv://root:root310@mern-book-store.umrmaf0.mongodb.net/?retryWrites=true&w=majority';
---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home'
+import CreateBook from './pages/CreateBook'
+import ShowBook from './pages/ShowBook'
+import editBook from './pages/editBook'
+import deleteBook from './pages/deleteBooks'
+
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/books/create' element={<CreateBook/>} />
+      <Route path='/books/details/:id' element={<ShowBook/>} />
+      <Route path='/books/edit/:id' element={<editBook/>} />
+      <Route path='/books/delete/:id' element={<deleteBook/>} />
+    </Routes>
+  )
+}
+
+export default App
